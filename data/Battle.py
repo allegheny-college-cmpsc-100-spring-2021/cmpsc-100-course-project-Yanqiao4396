96ec64a36e55e052ba7a49b81947e87311df630f
Yanqiao4396 <chen02@allegheny.edu>
2021-05-14 07:33:44 -0400

    project

---
+++ data/Battle.py
@@ -0,0 +1,77 @@
+import random
+class Battle:
+    def __init__(self, monster_a, monster_b):
+        self.monster_a = monster_a
+        self.monster_b = monster_b
+        
+    
+        
+           
+                
+    def demage(self):
+        if self.monster_a["hp"] < 0:
+         
+        demage = random.randint(15,30)
+        self.monster_b["hp"] -= demage
+        print(f"{self.monster_a} attacks {self.monster_b} with {demage} hp")
+        
+        
+            
+                
+    def winner(self):
+        if monster_a["hp"] > 0:
+            winner = monster_a
+        else:
+            winner = monster_b
+        print(f"{winner} wins!")
+        
+    def capture(self):
+        capture = random.random()
+        if capture <= self.monster_a["hp"] * 0.01:
+            print(f"{self.monster_a} tries to capture {self.monster_b}, but it fails")
+        else:
+            print(f"{self.monster_a} tries to capture {self.monster_b}..... it succeeds. The battle ends)
+            self.monster_b["hp"] -= 100
+           
+    
+    def heal(self):
+        self.monster_a["hp"] += 50
+        print(f"{self.monster_a} heals itself.")
+                  
+    def enemy(self):
+        demage = random.randint(15,30)
+        if self.monster_b["hp"] < 0:
+       
+        self.monster_a["hp"] -= demage
+        print(f"{self.monster_b} attacks {self.monster_a} with {demage} hp")
+    
+    def escape(self):
+        rate = random.random()
+        if rate >0.5 :
+            print(f"{self.monster_a} tries to escape but it fails")
+        else:
+            print(f"{self.monster_a} runs)
+            self.monster_a["hp"] -= 100
+         
+        
+   def fight(self):
+        while self.monster_a["hp"] > 0 and self.monster_b["hp"] > 0:
+            if self.monster_a["hp"] > 50:
+                self.demage()
+            elif self.monster_a["hp"] > 25:
+                  prob = random.random()
+                  if prob < 0.3:
+                  self.heal()
+                  else:
+                  self.demage()
+            else:
+                prob = random.random()
+                if prob < 0.3:
+                  self.heal()
+                elif prob < 0.6:
+                  self.escape()
+                else:
+                  self.demage()
+            self.enemy()
+        self.winner()
+        
--- docs/1_progress.md
+++ docs/1_progress.md
@@ -1,6 +1,6 @@
 # Course project progress
 
-`TODO: Your name here`
+Yanqiao Chen
 
 ## Using a Markdown `list`, detail the tasks you've completed and describe your progress.
 
@@ -8,7 +8,7 @@
 * this
 * list.
 
-`TODO`
+I have completed my most of my battle part. I have escape, turn, heal, attack systems now.
 
 ## Using a Markdown `list`, detail the tasks you still have yet to do. Where are you stuck? Where do you need help?
 
@@ -17,8 +17,8 @@
 * one
 * too.
 
-`TODO`
+In my program, the player's monster's behavior needs to be affected by its hp and enemy's hp. So I find I can only let its behavior directed by its own hp. So that means I can't make it try to capture enemy. Also, since my program is simple, the json you give me contains too much information. Can I just make a directary other than something like that one?
 
 ## Celebrate your successes. What are they?
 
-`TODO`
+At first, I thought the behaviors of two monsters are similar, so I wanted to put them into just one function just like what you did. But because I found that they should attack alternately and that player's enemy has more choices, I seperate them. In the matter of my capture system, I success in pushing it in a way that the less hp enemy has, the higher rate you have for capturing.
